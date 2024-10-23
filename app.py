9ca610cf8cda79114383600477c5bac6d9956164
gowtham-anvai <gowthambalaji344@gmail.com>
2024-10-23 16:36:50 +0530

    added req

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from pymongo import MongoClient
-import google.generativeai as genai
+# import google.generativeai as genai
 import os
 import uuid
 import tensorflow as tf
